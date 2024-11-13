de873a0986406c6d9bcc05e1c7c6713b8eca93d0
Antonin Rousseau <arousseau@student.hers.be>
2024-11-13 15:37:46 +0100

    refact: extract backstage pass logic in own function

--- GildedRose-Refactoring-Kata/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose-Refactoring-Kata/src/main/java/com/gildedrose/GildedRose.java
@@ -7,7 +7,7 @@ class GildedRose {
         this.items = items;
     }
 
-    public void updateRotting(Item item){
+    private void updateRotting(Item item){
         item.sellIn--;
         int dropRate = (item.sellIn < 0) ? 2 : 1;
         if(item.name.contains("Conjured")) dropRate = dropRate * 2;
@@ -17,20 +17,28 @@ class GildedRose {
         }
     }
 
-    public void updateSpecialCase(Item item){
+    private void updateBackstage(Item item){
+        if(item.sellIn < 0) {
+            item.quality = 0;
+        } else {
+            item.quality += 1;
+            if(item .sellIn <= 10) item.quality += 1;
+            if(item.sellIn <= 5) item.quality += 1;
+        }
+    }
+
+    private void updateSpecialCase(Item item){
         item.sellIn--;
-        int dropRate = (item.sellIn < 0) ? 2 : 1;
+        // Cancel if Sulfuras
         if(item.quality >= 50) return;
+
+        int dropRate = (item.sellIn < 0) ? 2 : 1;
         if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
-            if(item.sellIn < 0) {
-                item.quality = 0;
-            } else {
-                if(item .sellIn <= 10) item.quality += 2;
-                if(item.sellIn <= 5) item.quality += 1;
-            }
+            updateBackstage(item);
         } else {
             item.quality += 1 * dropRate;
         }
+        // No overflowing quality
         if(item.quality > 50) item.quality = 50;
     }
 
